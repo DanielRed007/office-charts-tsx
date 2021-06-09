2d1c5c793b0cc76377855257d5467faa1ac0288a
DanielRed007 <danielrojo1927@gmail.com>
2021-06-08 21:06:44 -0300

    first commit react app typescript material ui core implementation

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "@material-ui/core";
+
+const App = () => {
+  return (
+    <div>
+      Hello
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </div>
+  );
+};
+
+export default App;
