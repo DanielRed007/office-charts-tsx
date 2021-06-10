363f3a734368c0c8f8ec9444fc2de687a530c868
DanielRed007 <danielrojo1927@gmail.com>
2021-06-09 21:45:56 -0300

    backend initialized, concurrent dev environment setup, typescript implementation of backend done

---
+++ backend/server.ts
@@ -0,0 +1,18 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Api OK!");
+});
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} on port: ${port}`);
+});
