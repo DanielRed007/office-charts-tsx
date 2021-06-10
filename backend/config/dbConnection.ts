0010b1ccc3baf6f09e54b378b4b5ec0b6ea4f069
DanielRed007 <danielrojo1927@gmail.com>
2021-06-09 22:11:26 -0300

    front and dev envs fully operating, mongoDB setup complete linked to local instance

---
+++ backend/config/dbConnection.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const envs = {
+  local:
+    "mongodb://localhost:27017/?readPreference=primary&appname=MongoDB%20Compass&ssl=false",
+};
+
+export const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(envs.local, {
+      useUnifiedTopology: true,
+      useNewUrlParser: true,
+      useCreateIndex: true,
+    });
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(`MongoDB Error Office Supplies API : ${error.message}`);
+    process.exit(1);
+  }
+};
--- backend/server.ts
+++ backend/server.ts
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
+import { connectDB } from "./config/dbConnection";
 
 dotenv.config();
+connectDB();
 
 const app = express();
 
